ced4a665c18973e758e2bd95dfad27735281b870
MoggStephen <moggstephen66@gmail.com>
2023-03-24 13:36:09 +0000

    code complete

--- index.js
+++ index.js
@@ -1,60 +1,64 @@
+//Get elements
 const burgerBtn = document.querySelector(".burger-dropdown");
-const overlay = document.querySelector(".overlay-none");
 const linksContainer = document.querySelector(".links-container");
+const overlay = document.querySelector(".overlay-none");
 
+//Used to figure out if burger menu is collapsed or not
 let dropDownActive = false;
-burgerBtn.addEventListener("click", () => {
 
+
+//Burger icon is only visible in mobile
+//Replace classlist names when clicked on
+burgerBtn.addEventListener("click", () => {
     if (dropDownActive === false){
         dropDownActive = true;
-        overlay.classList.replace("overlay-none", "overlay-active");
-        burgerBtn.classList.replace("burger-dropdown", "burger-dropdown-active");
-        linksContainer.classList.replace("links-container", "links-container-active");
+        burgerBtn.classList.replace(burgerBtn.className, "burger-dropdown-active");
+        linksContainer.classList.replace(linksContainer.className, "links-container-active");
+        overlay.classList.replace(overlay.className, "overlay-active");
     }
     else if (dropDownActive === true){
         dropDownActive = false;
-        overlay.classList.replace("overlay-active", "overlay-none");
         burgerBtn.classList.replace("burger-dropdown-active", "burger-dropdown");
         linksContainer.classList.replace("links-container-active", "links-container");
+        overlay.classList.replace("overlay-active", "overlay-none");
     }
 })
 
-//TO fix issue with burger drop down menu being active and a user resizing the window
-//This leaves unwanted mobile screen size classnames
-//Event listener on window for when it resizes!
-//THis is abit overkill as you can just add the mobile classnames in the desktop and duplicate
-//  desktop classes and leave the user in the same state that they left it before they changed to desktop
+//When we click on a link in mobile we need to remove the active classnames to remove the dropdown menu!
+const links = document.querySelectorAll("nav a.link");
+links.forEach(link =>{
+  link.addEventListener("click", () =>{
 
-let prevScreenWidth = null;
-function toggleNavLinks() {
+    //Check if we are in mobile screenwidth
+    if (window.innerWidth < 1300) {
+      dropDownActive = false;
 
-  console.log(window.innerWidth)
-  if (window.innerWidth > prevScreenWidth && window.innerWidth > 1300 && window.innerWidth < 1315) {
-    let burgerBtn = null;
-    let linksContainer = null;
-    let overlay = null;
-  
-    //GET elements
-    //Factoring in different classnames that there could be
-    burgerBtn = document.querySelector(".burger-dropdown");
-    if (burgerBtn === null){
-      burgerBtn = document.querySelector(".burger-dropdown-active");
+      //RESET TO BASE CLASSNAMES
+      burgerBtn.classList.replace(burgerBtn.className, "burger-dropdown");
+      linksContainer.classList.replace(linksContainer.className, "links-container");
+      overlay.classList.replace(overlay.className, "overlay-none");
     }
-    linksContainer = document.querySelector(".links-container");
-    if(linksContainer === null){
-      linksContainer = document.querySelector(".links-container-active");
-    }
-    overlay = document.querySelector(".overlay-none");
-    if(overlay === null){
-      overlay = document.querySelector(".overlay-active");
-    }
+  })
+})
+
+//To fix issue with burger drop down menu being active and a user resizing the window
+//  this leaves unwanted mobile screen size classnames resulting in elements not being styled.
+
+//This is abit overkill as you can just add the mobile classnames outside of the media query and duplicate
+//  desktop screen size classes so there are no visual issues with the elements. This would result in
+//  the user resizing back to the mobile version and if they left with the menu active they would return to it active!
+
+//Event listener on window for when it resizes and change classnames where neccessary based of window.innerWidth
+let previousScreenWidth = null;
+window.addEventListener('resize', () =>{
+
+  //Is true when => (screen size increases && screensize is between 1300 and 1315)
+  if (window.innerWidth > previousScreenWidth && window.innerWidth > 1300 && window.innerWidth < 1315) {
 
     //RESET TO BASE CLASSNAMES
     burgerBtn.classList.replace(burgerBtn.className, "burger-dropdown");
     linksContainer.classList.replace(linksContainer.className, "links-container");
     overlay.classList.replace(overlay.className, "overlay-none");
   }
-  prevScreenWidth = window.innerWidth;
-}
-
-window.addEventListener('resize', toggleNavLinks);
+  previousScreenWidth = window.innerWidth;
+});
